1118cf32373089cff7df5bcd86c68d274fae0bd4
user140364 <user140364@74-80-245-200.ic2net.net>
2018-05-12 09:43:35 -0700

    feat(collection) se agrega grilla de collecciones junto a la tabla

---
+++ parqueape-mobile/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  parqueape-mobile
+//
+//  Created by user140364 on 5/12/18.
+//  Copyright © 2018 user140364. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
--- parqueape-mobile/ListParkingViewController.swift
+++ parqueape-mobile/ListParkingViewController.swift
@@ -18,7 +18,7 @@ extension ListParkingViewController: UITableViewDelegate, UITableViewDataSource 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -35,4 +35,33 @@ extension ListParkingViewController: UITableViewDelegate, UITableViewDataSource 
         
     }
     
+}
+
+extension ListParkingViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+
+        //cell.cellText.text = people[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (collectionView.frame.width - 20 ) / 3
+        return CGSize(width: width, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // esta funcion es para cuando le das click en la celda
+    }
 }
