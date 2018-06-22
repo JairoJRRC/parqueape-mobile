a050db04559a0b283017071ffba9ea468f48e6f1
Jairo Rojas <jairo.rafa.1997@gmail.com>
2018-06-22 09:25:49 -0700

    feat(mapa)se agrega ubicacion actual

--- parqueape-mobile/ViewController.swift
+++ parqueape-mobile/ViewController.swift
@@ -11,26 +11,70 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
     @IBOutlet weak var maps: MKMapView!
-    let regionRadius: CLLocationDistance = 1000
+    
+    
+    //let regionRadius: CLLocationDistance = 1000
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let inicialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
-        centerMapOnLocation(location: inicialLocation)
+        //let inicialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        //centerMapOnLocation(location: inicialLocation)
         // maps.showsUserLocation = true
+        
+        maps.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
     }
 
+    @IBAction func localizame(_ sender: Any) {
+        initLocation()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
-        maps.setRegion(coordinateRegion, animated: true)
+    //func centerMapOnLocation(location: CLLocation) {
+    //    let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+    //    maps.setRegion(coordinateRegion, animated: true)
+    //}
+    
+    func initLocation() {
+        
+        let permiso = CLLocationManager.authorizationStatus()
+        
+        if permiso == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        } else if permiso == .denied {
+            alertLocation(tit: "Error de localización", men: "Actualmente tiene denegada la localización del dispositivo.")
+        } else if permiso == .restricted {
+            alertLocation(tit: "Error de localización", men: "Actualmente tiene restringida la localización del dispositivo.")
+        } else {
+            
+            guard let currentCoordinate = locationManager.location?.coordinate else { return }
+            
+            let region = MKCoordinateRegionMakeWithDistance(currentCoordinate, 500 , 500)
+            maps.setRegion(region, animated: true)
+        }
     }
+    func alertLocation(tit: String, men: String) {
+        
+        let alerta = UIAlertController(title: tit, message: men, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        alerta.addAction(action)
+        self.present(alerta, animated: true, completion: nil)
+    }
+    
+}
 
+extension ViewController : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations[0])
+    }
 }
 
